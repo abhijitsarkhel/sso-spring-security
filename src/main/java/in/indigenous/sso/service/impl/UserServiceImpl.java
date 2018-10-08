0fcbbd6f25034ffa9f693cce864690c3c9edd73b
Abhijit Sarkhel <abhijit.s@SSLTUIODC.LOCAL>
2018-10-08 19:47:27 +0530

    Implementing UserServiceImpl

--- src/main/java/in/indigenous/sso/service/impl/UserServiceImpl.java
+++ src/main/java/in/indigenous/sso/service/impl/UserServiceImpl.java
@@ -1,23 +1,52 @@
 package in.indigenous.sso.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.authentication.UserCredentials;
 
+import in.indigenous.sso.model.Domain;
+import in.indigenous.sso.model.SubDomain;
+import in.indigenous.sso.repository.DomainRepository;
+import in.indigenous.sso.repository.SubDomainRepository;
 import in.indigenous.sso.security.dto.UserDTO;
 import in.indigenous.sso.security.dto.UserRole;
 import in.indigenous.sso.service.UserService;
 
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private DomainRepository domainRepository;
+	
+	@Autowired
+	private SubDomainRepository subDomainRepository;
 
 	@Override
 	public void registerDomain(String domain) {
-		// TODO Auto-generated method stub
-		
+		Domain dom = domainRepository.findByName(domain);
+		if(dom == null) {
+			// TODO - throw error
+		} else {
+			dom = new Domain();
+			dom.setName(domain);
+			domainRepository.save(dom);
+		}
 	}
 
 	@Override
 	public void registerSubDomain(String domain, String subDomain) {
-		// TODO Auto-generated method stub
-		
+		Domain dom = domainRepository.findByName(domain);
+		if(dom == null) {
+			// TODO - throw error
+		} else {
+			SubDomain subDom = subDomainRepository.findByDomainAndName(dom, subDomain);
+			if(subDom == null) {
+				// TODO - throw error
+			} else {
+				subDom = new SubDomain();
+				subDom.setDomain(dom);
+				subDom.setName(subDomain);
+				subDomainRepository.save(subDom);
+			}
+		}
 	}
 
 	@Override
